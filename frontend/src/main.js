f4c6a16284cafc3e797ace4675d632e382cc6762
skywardly <skywandou@gmail.com>
2021-06-12 18:43:38 +0900

    Add vue.jp related files

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,17 @@
 import Vue from "vue";
+
+// BootstrapVue
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
